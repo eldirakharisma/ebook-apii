d15bc62b67144eadb2d134697effb8f2ea032d85
eldirakharisma <eldirakharisma10@gmail.com>
2020-08-19 21:03:22 +0700

    Upload migration

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Routing\Controller as BaseController;
+
+class AuthController extends BaseController
+{
+    public function me()
+    {
+        return
+        [
+            "Nis" => "3103118053",
+            "Name" => "Eldira Kharisma ",
+            "Gender" => "Perempuan",
+            "Phone" => "085726337681",
+            "Kelas" => "XII RPL 2",
+        ];
+    }
+}
